43b8984d8099e640c814ea2d225a6bdf5c46fc6b
xcixor <pndungu54@gmail.com>
2023-10-12 18:36:49 +0300

    feat(auth): add clerk* auth

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+export default function Home() {
+  return (
+    <p className="text-3xl font-medium text-sky-700">
+      <UserButton afterSignOutUrl="/" />
+    </p>
+  );
+}
