3a6f82d4308ce58bf4a5a424df595f7ac34f1beb
xcixor <pndungu54@gmail.com>
2023-10-13 00:36:01 +0300

    chore(routes): change routes dynamically in the navbar

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <p className="text-3xl font-medium text-sky-700">
-      <UserButton afterSignOutUrl="/" />
-    </p>
+    <p className="text-3xl font-medium text-sky-700">This is your dashboard</p>
   );
 }
