05da5901c5abf19c5c96e1f483c23fd832e30206
xcixor <pndungu54@gmail.com>
2023-10-13 00:01:45 +0300

    chore(navigation): add navbar

---
+++ components/NavbarRoutes.tsx
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+export default function NavbarRoutes() {
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      <UserButton />
+    </div>
+  );
+}
