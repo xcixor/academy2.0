fe5a6ce9d76401b1b9c6923e4b9e98616d96c380
xcixor <pndungu54@gmail.com>
2023-10-13 14:37:05 +0300

    feat(courses): create course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 }
