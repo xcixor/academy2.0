a97a392cac504c4ea4b571aff90ec3ebea15b5d7
xcixor <pndungu54@gmail.com>
2023-10-14 21:27:51 +0300

    feat(search): implement search

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -6,15 +6,22 @@ import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { SearchInput } from "./SearchInput";
 
 export default function NavbarRoutes() {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/courses");
+  const isSearchPage = pathname === "/search";
 
   return (
     <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isPlayerPage ? (
           <Link href="/">
