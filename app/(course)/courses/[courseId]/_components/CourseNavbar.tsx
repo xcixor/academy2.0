6b24226dce9b9d0dd73da7c053e2aaf793d0e1b7
xcixor <pndungu54@gmail.com>
2023-10-15 03:38:37 +0300

    feat(course): add course preview layout

---
+++ app/(course)/courses/[courseId]/_components/CourseNavbar.tsx
@@ -0,0 +1,26 @@
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+import NavbarRoutes from "@/components/NavbarRoutes";
+
+import CourseMobileSidebar from "./CourseMobileSidebar";
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+export default function CourseNavbar({
+  course,
+  progressCount,
+}: CourseNavbarProps) {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
+      <NavbarRoutes />
+    </div>
+  );
+}
