d42c667f84c451549a8408685068fa64e500f527
xcixor <pndungu54@gmail.com>
2023-10-16 09:10:38 +0300

    feat(course): add Add to plan form in course edit page to save course plan to database

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 
 const database = new PrismaClient();
 
-async function main() {
+async function createCategories() {
   try {
     await database.category.createMany({
       data: [
@@ -22,6 +22,25 @@ async function main() {
   } finally {
     await database.$disconnect();
   }
+}
+
+async function createPlans() {
+  try {
+    await database.plan.createMany({
+      data: [{ name: "Free" }, { name: "Basic" }, { name: "Premium" }],
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database plans", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+async function main() {
+  createCategories()
+  createPlans()
 }
 
 main();
