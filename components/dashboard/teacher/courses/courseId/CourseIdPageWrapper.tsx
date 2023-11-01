470232e1d04dad0870eaeb0dee711e6fac0d2e59
xcixor <pndungu54@gmail.com>
2023-11-01 20:01:22 +0300

    refactor(course setup page): lazy load setup page

---
+++ components/dashboard/teacher/courses/courseId/CourseIdPageWrapper.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import CourseIdPage from "./CourseIdPage";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+interface Props {
+  courseId: string;
+  userId: string;
+}
+
+const CourseIdPageWrapper = async ({ courseId, userId }: Props) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: courseId,
+      userId,
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+      chapters: {
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  const plans = await db.plan.findMany();
+  return <CourseIdPage course={course} categories={categories} plans={plans} />;
+};
+
+export default CourseIdPageWrapper;
