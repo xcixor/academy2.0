6d8d08a10519b6643304197584fd4957fb3e93fd
xcixor <pndungu54@gmail.com>
2023-11-01 20:43:24 +0300

    refactor(loading): add missing skeletons

--- app/dashboard/(routes)/(root)/page.tsx
+++ app/dashboard/(routes)/(root)/page.tsx
@@ -1,51 +1,20 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { CheckCircle, Clock } from "lucide-react";
-import { getDashboardCourses } from "@/actions/get-dashboard-courses";
-import CoursesList from "@/components/CoursesList";
-import InfoCard from "../../../../components/dashboard/root/InfoCard";
-import { Suspense, lazy } from "react";
-import DashboardSkeleton from "../../../../components/dashboard/root/DashboardSkeleton";
+import DashboardSkeleton from "@/components/dashboard/root/DashboardSkeleton";
+import dynamic from "next/dynamic";
 
-const LoadingFallback = () => <DashboardSkeleton />;
+const DashboardPageCustomLoading = dynamic(
+  () => import("@/components/dashboard/root/DashboardPageWrapper"),
+  {
+    loading: () => <DashboardSkeleton />,
+  }
+);
 
-const SuspenseDashboard = lazy(async () => {
+export default function Dashboard() {
   const { userId } = auth();
 
   if (!userId) {
     redirect("/");
   }
-
-  const { completedCourses, coursesInProgress } = await getDashboardCourses(
-    userId
-  );
-
-  return {
-    default: () => (
-      <div className="p-6 space-y-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <InfoCard
-            icon={Clock}
-            label="In Progress"
-            numberOfItems={coursesInProgress.length}
-          />
-          <InfoCard
-            icon={CheckCircle}
-            label="Completed"
-            numberOfItems={completedCourses.length}
-            variant="success"
-          />
-        </div>
-        <CoursesList items={[...coursesInProgress, ...completedCourses]} />
-      </div>
-    ),
-  };
-});
-
-export default function Dashboard() {
-  return (
-    <Suspense fallback={<LoadingFallback />}>
-      <SuspenseDashboard />
-    </Suspense>
-  );
+  return <DashboardPageCustomLoading userId={userId} />;
 }
