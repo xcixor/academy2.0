99bdcaf528beee9ae254ae67d565f040c115c6de
xcixor <pndungu54@gmail.com>
2023-11-06 07:11:01 +0300

    chore(search): enhance search page

--- components/CoursesList.tsx
+++ components/CoursesList.tsx
@@ -1,6 +1,7 @@
 import { Category, Course } from "@prisma/client";
 
 import CourseCard from "@/components/CourseCard";
+import { getAllCourses } from "@/actions/get-all-courses";
 
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
@@ -12,7 +13,18 @@ interface CoursesListProps {
   items: CourseWithProgressWithCategory[];
 }
 
-export default function CoursesList({ items }: CoursesListProps) {
+interface PageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function CoursesList({ searchParams }: PageProps) {
+  const items = (await getAllCourses({
+    ...searchParams,
+  })) as CourseWithProgressWithCategory[];
+
   return (
     <div>
       <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
