64fe0154fa8201c4d8ac463c2200b67e490cf5ff
xcixor <pndungu54@gmail.com>
2023-11-13 14:45:38 +0300

    Add routes for publishing and unpublishing questions in quizzes and implement corresponding API endpoints and UI actions.
    Changes include adding new files for publishing and unpublishing routes,
    updating the PATCH handler function in these routes to check for user
    authorization and update the isPublished field of the question, and
    updating the QuestionActions component to make API calls to publish or
    unpublish a question in a quiz.

---
+++ app/api/courses/[courseId]/quizzes/[quizId]/questions/[questionId]/publish/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import { getLoggedInUser } from "@/lib/auth/utils";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: { params: { courseId: string; quizId: string; questionId: string } }
+) {
+  try {
+    const user = await getLoggedInUser();
+    const userId = user?.userId;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const publishedQuestion = await db.question.update({
+      where: {
+        id: params.questionId,
+        quizId: params.quizId,
+      },
+      data: {
+        isPublished: true,
+      },
+    });
+
+    return NextResponse.json(publishedQuestion);
+  } catch (error) {
+    console.log("[CHAPTER_PUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
