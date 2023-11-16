0783ca7c64d5c99e8c4edfdbb75f9825c46907ea
xcixor <pndungu54@gmail.com>
2023-11-16 20:53:47 +0300

    feat(chapters): mark taken quizes in chapter page

--- components/courses/courseId/quizzes/QuizList.tsx
+++ components/courses/courseId/quizzes/QuizList.tsx
@@ -1,5 +1,6 @@
+import { getLoggedInUser } from "@/lib/auth/utils";
 import { db } from "@/lib/db";
-import { ChevronsRight } from "lucide-react";
+import { AlertTriangle, CheckCircle2, ChevronsRight } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -8,23 +9,46 @@ type Props = {
 };
 
 const QuizList = async ({ courseId }: Props) => {
+  const user = await getLoggedInUser();
+  const userId = user?.userId;
+
+  if (!userId) {
+    return (
+      <div className="flex items-center justify-center w-full">
+        <AlertTriangle className="h-8 w-8 text-red-500" />
+      </div>
+    );
+  }
+
   const quizzes = await db.quiz.findMany({
     where: {
       courseId: courseId,
       isPublished: true,
     },
+    include: {
+      submissions: {
+        where: {
+          userId: userId,
+        },
+      },
+    },
   });
 
   return (
-    <div className="p-4">
+    <div className="flex flex-col gap-4">
       {quizzes.map((quiz) => (
         <Link
           href={`/courses/${courseId}/quizzes/${quiz.id}`}
           key={quiz.id}
           target="_blank"
         >
-          <span className="font-semibold text-sky-700 hover:underline hover:text-sky-500 flex items-center">
-            {quiz.title} <ChevronsRight />
+          <span className="flex items-center justify-between ">
+            <span className="font-semibold text-sky-700 hover:underline hover:text-sky-500 flex items-center">
+              {quiz.title} <ChevronsRight />
+            </span>
+            {quiz.submissions[0] && (
+              <CheckCircle2 className="h-4 w-4 text-green-400" />
+            )}
           </span>
         </Link>
       ))}
