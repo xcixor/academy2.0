34b6bec291bf656a3f968ceaac21dda9539165c1
xcixor <pndungu54@gmail.com>
2023-11-17 13:20:19 +0300

    feat: Add submission table for teacher quizzes
    This commit introduces a new submission table component to display
    student submissions and grades for a teacher's quizzes. The addition
    includes the following files:
    - `page.tsx` in the `/app/dashboard/(routes)/teacher/quizzes/[quizId]/`
    directory: Implements the main page component that renders the
    submission table and passes the quizId as a prop to the table.
    - `Columns.tsx` in the `/components/dashboard/teacher/quizzes/quizId/`
    directory: Defines the columns for the submission table, including
    sorting functionality and custom cell rendering.
    - `DataTable.tsx` in the `/components/dashboard/teacher/quizzes/quizId/`
    directory: Implements the main table component that handles filtering,
    sorting, and pagination of the submission data.
    - `SubmissionsTable.tsx` in the
    `/components/dashboard/teacher/quizzes/quizId/` directory: Contains the
    logic to fetch and calculate submission grades, and renders the
    submission table with the fetched data.
    
    The addition of the submission table provides teachers with a convenient
    way to review and grade student submissions for their quizzes.

---
+++ components/dashboard/teacher/quizzes/quizId/SubmissionsTable.tsx
@@ -0,0 +1,101 @@
+import { getLoggedInUser } from "@/lib/auth/utils";
+import { db } from "@/lib/db";
+import { notFound, redirect } from "next/navigation";
+import React from "react";
+import { DataTable } from "./DataTable";
+import { columns } from "./Columns";
+
+type Props = {
+  quizId: string;
+};
+
+type SubmissionProps = {
+  studentName: string;
+  studentScore: number;
+  passed: boolean;
+};
+
+async function gradeStudent(
+  quizId: string,
+  studentId: string,
+  totalQuestions: number,
+  passingPercentage: number
+) {
+  const user = await db.user.findUnique({
+    where: { id: studentId },
+    include: { responses: { include: { question: true, option: true } } },
+  });
+  //   console.log(user?.responses);
+  const correctMarks = user?.responses.reduce((acc, curr) => {
+    if (curr.option.isAnswer && curr.question.quizId === quizId) {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+  if (correctMarks && correctMarks >= 1) {
+    const studentScore = (correctMarks / totalQuestions) * 100;
+    return {
+      passed: studentScore > passingPercentage,
+      marks: studentScore,
+    };
+  }
+  return {
+    passed: false,
+    marks: 0,
+  };
+}
+
+const SubmissionsTable = async ({ quizId }: Props) => {
+  const user = await getLoggedInUser();
+  const studentName = user?.userId;
+
+  if (!studentName) {
+    return redirect("/");
+  }
+
+  const quiz = await db.quiz.findUnique({
+    where: {
+      id: quizId,
+    },
+    include: {
+      course: true,
+      submissions: { include: { user: true } },
+      questions: true,
+    },
+  });
+  if (!quiz) {
+    return notFound();
+  }
+
+  const courseTitle = quiz.course.title;
+  const submissionList: SubmissionProps[] = [];
+
+  for (const submission of quiz.submissions) {
+    const grade = await gradeStudent(
+      submission.quizId,
+      submission.userId,
+      quiz.questions.length,
+      quiz.passingPercentage
+    );
+    submissionList.push({
+      studentName: submission.user.name || submission.user.email || "Not Found",
+      studentScore: grade.marks,
+      passed: grade.passed,
+    });
+  }
+
+  console.log(submissionList);
+
+  return (
+    <div>
+      <div>
+        <p>Quiz {quiz.title}</p>
+        <p>Course: {courseTitle}</p>
+      </div>
+      <DataTable columns={columns} data={submissionList} />
+    </div>
+  );
+};
+
+export default SubmissionsTable;
