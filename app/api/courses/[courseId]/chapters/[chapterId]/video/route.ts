dacd5ec3fca1b5ef28ebf2d7801eee775a8bd637
xcixor <pndungu54@gmail.com>
2023-11-28 14:38:39 +0300

    Add video upload functionality for gcp and error handling
    New route for uploading videos, error logging, and video form changes
    for uploading videos in the teacher dashboard. Also, an association
    between chapters and GCP data was added in the Prisma schema. These
    changes aim to enhance the platform's capability to upload, manage, and
    display videos for course chapters.

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/video/route.ts
@@ -0,0 +1,89 @@
+import { NextResponse } from "next/server";
+import { File } from "form-data";
+import { db } from "@/lib/db";
+import { getLoggedInUser } from "@/lib/auth/utils";
+import { FileUploader } from "@/lib/gcp";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } },
+) {
+  try {
+    const user = await getLoggedInUser();
+    const userId = user?.userId;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const formData = await req.formData();
+    const data = Object.fromEntries(formData);
+    const { file }: { file: File } = data as { file: File };
+
+    let blobName = `courses/${params.courseId}/chapters/${params.chapterId}/video/${file.name}`;
+    const contentType: string = file.type;
+    const downloadExpiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const uploader = new FileUploader(
+      blobName,
+      contentType,
+      "PUT",
+      downloadExpiryDate,
+    );
+
+    const cloudResponse = await uploader.uploadFile(file);
+
+    if (cloudResponse.status !== 200) {
+      return new NextResponse("Internal Error", { status: 500 });
+    }
+
+    const existingGCPVideo = await db.gCPData.findFirst({
+      where: {
+        assetId: params.chapterId,
+      },
+    });
+
+    if (existingGCPVideo) {
+      await db.gCPData.delete({
+        where: {
+          id: existingGCPVideo.id,
+        },
+      });
+    }
+
+    const gcpData = await db.gCPData.create({
+      data: {
+        assetId: params.chapterId,
+        urlExpiryDate: downloadExpiryDate,
+        blobName: blobName,
+        assetName: file.name,
+        assetType: contentType,
+      },
+    });
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+      },
+      data: {
+        gCPDataId: gcpData.id,
+        videoUrl: cloudResponse.downloadUrl,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("COURSE_ID_ATTACHMENTS", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- components/dashboard/teacher/courses/courseId/chapters/chapterId/ChapterVideoForm.tsx
+++ components/dashboard/teacher/courses/courseId/chapters/chapterId/ChapterVideoForm.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import * as z from "zod";
-import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
 import { Pencil, PlusCircle, Video } from "lucide-react";
 import { useState } from "react";
-import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Chapter, MuxData } from "@prisma/client";
 
@@ -18,10 +16,6 @@ interface ChapterVideoFormProps {
   chapterId: string;
 }
 
-const formSchema = z.object({
-  videoUrl: z.string().min(1),
-});
-
 export default function ChapterVideoForm({
   initialData,
   courseId,
@@ -31,37 +25,23 @@ export default function ChapterVideoForm({
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
-  const router = useRouter();
-
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try {
-      await axios.patch(
-        `/api/courses/${courseId}/chapters/${chapterId}`,
-        values
-      );
-      toast.success("Chapter updated");
-      toggleEdit();
-      router.refresh();
-    } catch {
-      toast.error("Something went wrong");
-    }
-  };
+  const uploadUrl = `/api/courses/${courseId}/chapters/${chapterId}/video`;
 
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
-      <div className="font-medium flex items-center justify-between">
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
         Chapter video
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
           {!isEditing && !initialData.videoUrl && (
             <>
-              <PlusCircle className="h-4 w-4 mr-2" />
+              <PlusCircle className="mr-2 h-4 w-4" />
               Add a video
             </>
           )}
           {!isEditing && initialData.videoUrl && (
             <>
-              <Pencil className="h-4 w-4 mr-2" />
+              <Pencil className="mr-2 h-4 w-4" />
               Edit video
             </>
           )}
@@ -69,31 +49,28 @@ export default function ChapterVideoForm({
       </div>
       {!isEditing &&
         (!initialData.videoUrl ? (
-          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+          <div className="flex h-60 items-center justify-center rounded-md bg-slate-200">
             <Video className="h-10 w-10 text-slate-500" />
           </div>
         ) : (
-          <div className="relative aspect-video mt-2">
+          <div className="relative mt-2 aspect-video">
             <MuxPlayer playbackId={initialData?.muxData?.playbackId || ""} />
           </div>
         ))}
       {isEditing && (
         <div>
           <FileUpload
-            endpoint="chapterVideo"
-            onChange={(url) => {
-              if (url) {
-                onSubmit({ videoUrl: url });
-              }
-            }}
+            uploadUrl={uploadUrl}
+            isFileEditing={false}
+            toggleEdit={toggleEdit}
           />
-          <div className="text-xs text-muted-foreground mt-4">
+          <div className="mt-4 text-xs text-muted-foreground">
             Upload this chapter&apos;s video
           </div>
         </div>
       )}
       {initialData.videoUrl && !isEditing && (
-        <div className="text-xs text-muted-foreground mt-2">
+        <div className="mt-2 text-xs text-muted-foreground">
           Videos can take a few minutes to process. Refresh the page if video
           does not appear.
         </div>
