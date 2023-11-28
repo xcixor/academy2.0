3889a3e01619cf72557bca6c7b1a58ec27b0b559
xcixor <pndungu54@gmail.com>
2023-11-28 16:25:31 +0300

    Refactor loading and page components
    Simplify the page component by directly importing CourseChapter, and add
    a new Loading component to handle loading states and display skeleton
    UI. This streamlines the code and improves maintainability.

---
+++ app/(course)/courses/[courseId]/(chapters)/chapters/[chapterId]/loading.tsx
@@ -0,0 +1,6 @@
+import CoursePageSkeleton from "@/components/courses/courseId/chapters/chapterId/CoursePageSkeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <CoursePageSkeleton />;
+}
