1e7a748ec077a0d1d714aef32beea9049965c804
xcixor <pndungu54@gmail.com>
2023-11-29 10:52:20 +0300

    Refactor user model and add new instructor page component
    Refactor user model to include additional fields for social media and
    roles. Add new page component for displaying instructor details. This
    improves user data representation and expands frontend functionality. No
    associated issues.

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -3,14 +3,12 @@ import { Navbar } from "@/components/navbar/Navbar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="flex h-full flex-col ">
       <div className="fixed inset-y-0 z-50 h-[80px] w-full">
         <Navbar />
       </div>
-      <main className="h-full pt-[80px]">
-        {children}
-        <Footer />
-      </main>
+      <main className="flex-1 pt-[80px]">{children}</main>
+      <Footer />
     </div>
   );
 };
