b52ea970c62227fb308d204b3ff2bcb3d57a5df8
xcixor <pndungu54@gmail.com>
2023-12-19 14:17:50 +0300

    add function to create admin user

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const { PrismaClient } = require("@prisma/client");
 
 const database = new PrismaClient();
@@ -37,10 +39,31 @@ async function createPlans() {
   }
 }
 
+async function createAdmin() {
+  try {
+    const adminPass = process.env.ADMIN_PASSWORD;
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const adminName = process.env.ADMIN_EMAIL;
+    const hashPassword = await bcrypt.hash(adminPass, 10);
+    await database.user.create({
+      data: {
+        name: adminName,
+        password: hashPassword,
+        email: adminEmail,
+        isAdmin: true,
+      },
+    });
+  } catch (error) {
+    console.log("Error creating db admin.", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
 
 async function main() {
   createCategories();
   createPlans();
+  createAdmin();
 }
 
 main();
