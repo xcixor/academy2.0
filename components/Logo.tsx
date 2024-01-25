d93176d175ded6941d8c1cb0674f36ebc6944b1c
bryanaim <isale.bryan@gmail.com>
2024-01-25 12:04:11 +0300

    Update Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image height={130} width={130} alt="logo" src="/logo.png" />;
+  return <Image height={130} width={130} alt="PES Academy logo" src="/PES-Academy-logo.png" />;
 };
