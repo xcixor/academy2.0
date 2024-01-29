5a95eac9ad1273380fc8f3bc0610d08098b75335
xcixor <pndungu54@gmail.com>
2024-01-29 14:51:29 +0300

    Refactor CoursesList component and related files
    - Move CourseWithProgressWithCategory type to separate file
    - Import CourseWithProgressWithCategory type in relevant files
    - Update CoursesList component to accept items prop instead of searchParams prop
    - Update CoursesList component to use items prop instead of fetching data again

--- components/CoursesList.tsx
+++ components/CoursesList.tsx
@@ -1,30 +1,11 @@
-import { Category, Course } from "@prisma/client";
-
 import CourseCard from "@/components/CourseCard";
-import { getAllCourses } from "@/actions/get-all-courses";
-
-type CourseWithProgressWithCategory = Course & {
-  category: Category | null;
-  chapters: { id: string }[];
-  progress?: number | null;
-};
+import { CourseWithProgressWithCategory } from "@/@types/db";
 
 interface CoursesListProps {
   items: CourseWithProgressWithCategory[];
 }
 
-interface PageProps {
-  searchParams: {
-    title: string;
-    categoryId: string;
-  };
-}
-
-export default async function CoursesList({ searchParams }: PageProps) {
-  const items = (await getAllCourses({
-    ...searchParams,
-  })) as CourseWithProgressWithCategory[];
-
+export default async function CoursesList({ items }: CoursesListProps) {
   return (
     <div>
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
@@ -34,9 +15,9 @@ export default async function CoursesList({ searchParams }: PageProps) {
             id={item.id}
             title={item.title}
             chaptersLength={item.chapters.length}
-            price={item.price!}
             progress={item.progress}
-            category={item?.category?.name!}
+            category={item?.category?.name}
+            price={item.price}
           />
         ))}
       </div>
