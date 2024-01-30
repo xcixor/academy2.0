ee5357e6b41c072e400bad4b9cd818f58cfaee2a
xcixor <pndungu54@gmail.com>
2024-01-30 12:18:11 +0300

    Refactor uploadFile function and import getFileExtension
    This commit refactors the `uploadFile` function in the `lib/gcp/gcp-utils.ts` file. It imports the `getFileExtension` function from the `lib/files/index.ts` file and uses it to determine the content type of the file being uploaded. This change improves the accuracy of the content type and ensures that the correct MIME type is used.

--- lib/gcp/gcp-utils.ts
+++ lib/gcp/gcp-utils.ts
@@ -1,3 +1,4 @@
+import { getFileExtension } from "../files";
 import { FileUploader } from "./gcp";
 
 const UPLOAD_EXPIRY_IN_SECONDS = new Date(Date.now() + 24 * 60 * 60 * 1000);
@@ -6,7 +7,8 @@ export const DOWNLOAD_EXPIRY_IN_SECONDS = new Date(
 );
 
 export async function uploadFile(file: File, fileName: string) {
-  const contentType: string = file.type;
+  const contentType: string = getFileExtension(file);
+  console.log(contentType,'yipeee')
   const downloadExpiryDate = UPLOAD_EXPIRY_IN_SECONDS;
   const uploader = new FileUploader(
     fileName,
