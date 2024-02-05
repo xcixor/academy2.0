3226c0a6627f85f4877e169b24122600fcc9b79a
bryanaim <isale.bryan@gmail.com>
2024-02-05 12:56:55 +0300

    remove meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,6 @@ import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: "PES Academy",
-    template: `%s | PES Academy`
-  },
-  description: "Join us on a transformative journey at PES Academy, where innovation meets education, developing the leaders of tomorrow's successful enterprises.",
-};
 
 export default function RootLayout({
   children,
