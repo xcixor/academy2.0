dc1a0c5d16b0e5813afd82a6c06e5849a1da6952
bryanaim <isale.bryan@gmail.com>
2024-02-09 13:16:36 +0300

    Define PES red & Pes Blue color values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,8 @@ module.exports = withUt({
     },
     extend: {
       colors: {
+        'pes-red': '#A31A2A',
+        'pes-blue': '#4B6386',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
