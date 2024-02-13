f60f63f3ba203d7f67076c799357d4f7e8e191b9
xcixor <pndungu54@gmail.com>
2024-02-13 21:32:39 +0300

    Refactor video thumbnail generation and uploading
    - Refactored the `createVideoThumbnails` function to accept additional parameters for the destination folder.
    - Updated the `generateThumbnails` function to pass the destination folder to the `createVideoThumbnails` function.
    - Added error handling and logging for thumbnail generation and uploading.
    - Updated the `uploadToGCPCloud` function to return the public URL of the uploaded file.
    - Created a new function `generateVTT` to generate VTT content and append it to an existing VTT file.
    - Added functions `getFileContent` and `writeFileContent` to handle reading and writing file content from GCP.
    - Updated the `uploadToGCPCloud` function to use the new functions for generating and updating VTT files.
    - Exported the `uploadToGCPCloud` and `generateVTT` functions from the `uploader.ts` module.

--- lib/video/thumbnail.ts
+++ lib/video/thumbnail.ts
@@ -1,38 +1,31 @@
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
-const uploadToGCPCloud = require("./uploader.ts");
+const { uploadToGCPCloud, generateVTT } = require("./uploader.ts");
 const filePath = "/home/xcixor/Videos/comp.mp4";
 
-// function createVideoThumbnail(filePath) {
-//   const outputPath = Date.now() + "thumbnail.png";
-//   const outStream = fs.createWriteStream(outputPath);
-
-//   const time = `00:00:15`;
-//   ffmpeg(filePath)
-//     .seekInput(time)
-//     .format("mjpeg") // -f mjpeg
-//     .frames(1) // -vframes 1
-//     // .size("1920x1080") // -s 320x240 : w = 320, h = 240
-//     .on("error", function (err) {
-//       console.log("An error occurred: " + err.message);
-//     })
-//     .on("end", function () {
-//       console.log("Processing finished !");
-//     })
-//     .pipe(outStream, { end: true });
-// }
-
-function createVideoThumbnails(filePath, numThumbnails, duration, i) {
-  return new Promise((resolve, reject) => {
+let captions = new Array();
+function createVideoThumbnails(
+  filePath,
+  numThumbnails,
+  duration,
+  i,
+  destinationFolder,
+) {
+  return new Promise(async (resolve, reject) => {
     const outputPath = Date.now() + "thumbnail.png";
 
     const interval = duration / numThumbnails;
 
-    let time = formatTime(i * interval);
+    let time;
 
-    if (i === 0) {
+    if (parseInt(i) === 0) {
       time = "00:00:01";
+    } else {
+      time = formatTime(i * interval);
     }
+    let startTime = time;
+    let endTime = formatTime((i + 1) * interval);
+
     const ffmpegCommand = ffmpeg(filePath)
       .seekInput(time)
       .format("mjpeg")
@@ -42,12 +35,14 @@ function createVideoThumbnails(filePath, numThumbnails, duration, i) {
       })
       .on("end", function () {
         console.log("Processing finished for thumbnail " + (i + 1));
-        resolve(outputPath);
       });
     const stream = ffmpegCommand.pipe();
-    const gsLocation = process.env.GS_LOCATION;
-    const destination = `${gsLocation}/test/${outputPath}`;
-    uploadToGCPCloud(stream, destination);
+
+    const destination = `${destinationFolder}/${outputPath}`;
+    const url = await uploadToGCPCloud(stream, destination);
+    const fileName = `${destinationFolder}/captions.vtt`;
+    await generateVTT(startTime, endTime, url, fileName);
+    resolve(outputPath);
   });
 }
 
@@ -85,17 +80,27 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function generateThumbnails() {
+async function generateThumbnails(gcpDestinationFolder) {
   const duration = await getVideoDuration(filePath);
-  console.log(duration);
+
   try {
     for (let i = 0; i < numThumbnails; i++) {
-      createVideoThumbnails(filePath, numThumbnails, duration, i);
-      await sleep(500);
+      const gsLocation = process.env.GS_LOCATION;
+      const destinationFolder = `${gsLocation}/${gcpDestinationFolder}`;
+      const result = await createVideoThumbnails(
+        filePath,
+        numThumbnails,
+        duration,
+        i,
+        destinationFolder,
+      );
+      console.log(result);
+      // await sleep(200);
     }
   } catch (err) {
     console.log("An error occurred: " + err.message);
+  } finally {
   }
 }
 
-generateThumbnails();
+generateThumbnails("test");
