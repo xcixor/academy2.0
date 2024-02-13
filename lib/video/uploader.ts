c9f7e01e390273b4c347e4df550905234e0f7633
xcixor <pndungu54@gmail.com>
2024-02-13 21:32:39 +0300

    Refactor video thumbnail generation and uploading
    - Refactored the `createVideoThumbnails` function to accept additional parameters for the destination folder.
    - Updated the `generateThumbnails` function to pass the destination folder to the `createVideoThumbnails` function.
    - Added error handling and logging for thumbnail generation and uploading.
    - Updated the `uploadToGCPCloud` function to return the public URL of the uploaded file.
    - Created a new function `generateVTT` to generate VTT content and append it to an existing VTT file.
    - Added functions `getFileContent` and `writeFileContent` to handle reading and writing file content from GCP.
    - Updated the `uploadToGCPCloud` function to use the new functions for generating and updating VTT files.
    - Exported the `uploadToGCPCloud` and `generateVTT` functions from the `uploader.ts` module.

--- lib/video/uploader.ts
+++ lib/video/uploader.ts
@@ -2,65 +2,27 @@ const { Storage } = require("@google-cloud/storage");
 const path = require("path");
 const gsBucketName = process.env.GS_PUBLIC_BUCKET_NAME;
 const gsLocation = process.env.GS_LOCATION;
-console.log(gsBucketName, gsLocation);
 // Initialize storage
 const storage = new Storage({ keyFilename: process.env.GS_CREDENTIALS });
 const bucket = storage.bucket(gsBucketName);
-// Sending the upload request
-
-// function uploadToGCPCloud(fileToUpload, destination) {
-//   bucket.upload(
-//     fileToUpload,
-//     {
-//       destination: destination,
-//     },
-//     function (err, file) {
-//       if (err) {
-//         console.error(`Error uploading image image_to_upload.jpeg: ${err}`);
-//       } else {
-//         console.log(`Image image_to_upload.jpeg uploaded to ${gsBucketName}.`);
-//         console.log(file.publicUrl());
-//         // Making file public to the internet
-//         file.makePublic(async function (err) {
-//           if (err) {
-//             console.error(`Error making file public: ${err}`);
-//           } else {
-//             console.log(`File ${file.name} is now public.`);
-//             const publicUrl = file.publicUrl();
-//             console.log(`Public URL for ${file.name}: ${publicUrl}`);
-//           }
-//         });
-//       }
-//     },
-//   );
-// }
-// const fileToUpload = `/home/xcixor/Pictures/bsaka.jpeg`;
-// const fileName = path.basename(fileToUpload);
-// const gcpFile = `${gsLocation}/test/${fileName}`;
-
-// uploadToGCPCloud(fileToUpload, gcpFile);
-
 
 function uploadToGCPCloud(stream, destination) {
   return new Promise((resolve, reject) => {
     const file = bucket.file(destination);
     const writeStream = file.createWriteStream();
 
-    stream.pipe(writeStream)
-      .on('error', (err) => {
+    stream
+      .pipe(writeStream)
+      .on("error", (err) => {
         reject(err);
       })
-      .on('finish', () => {
-        console.log(`Image uploaded to ${gsBucketName}.`);
-        console.log(file.publicUrl());
+      .on("finish", () => {
         // Making file public to the internet
         file.makePublic(async function (err) {
           if (err) {
             reject(err);
           } else {
-            console.log(`File ${file.name} is now public.`);
             const publicUrl = file.publicUrl();
-            console.log(`Public URL for ${file.name}: ${publicUrl}`);
             resolve(publicUrl);
           }
         });
@@ -68,5 +30,54 @@ function uploadToGCPCloud(stream, destination) {
   });
 }
 
-module.exports = uploadToGCPCloud;
+// Cloud Function entry point
+async function generateVTT(startTime, endTime, url, fileName) {
+  try {
+    // Generate the VTT content
+    const vttContent = generateVTTContent(startTime, endTime, url);
+    // Get the existing VTT file content from GCP
+    const existingContent = await getFileContent(gsBucketName, fileName);
 
+    // Append the new VTT content to the existing content
+    const updatedContent = existingContent + vttContent;
+    // Write the updated VTT content to the GCP file
+    await writeFileContent(gsBucketName, fileName, updatedContent);
+
+    // console.log("VTT file created successfully!");
+  } catch (error) {
+    console.error("Error generating VTT file:", error);
+  }
+}
+
+// Function to get the existing file content from GCP
+async function getFileContent(bucketName, fileName) {
+  const file = storage.bucket(bucketName).file(fileName);
+
+  try {
+    const [content] = await file.download();
+    return content.toString();
+  } catch (error) {
+    if (error.code === 404) {
+      console.log(`File ${fileName} not found in bucket ${bucketName}`);
+      return "";
+    }
+    throw error;
+  }
+}
+
+// Function to write the updated file content to GCP
+async function writeFileContent(gsBucketName, fileName, content) {
+  const file = storage.bucket(gsBucketName).file(fileName);
+  await file.save(content, {
+    contentType: "text/vtt",
+    resumable: false,
+    validation: false,
+  });
+}
+
+// Function to generate the VTT content
+function generateVTTContent(startTime, endTime, url) {
+  return `${startTime} --> ${endTime}\n${url}\n\n`;
+}
+
+module.exports = { uploadToGCPCloud, generateVTT };
