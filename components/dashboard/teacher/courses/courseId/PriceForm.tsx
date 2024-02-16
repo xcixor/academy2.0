863d6c89eb1cff6f1a15d96ef3390f50aafe03f9
xcixor <pndungu54@gmail.com>
2024-02-16 09:25:10 +0300

    feat(course): Add course access functionality and update price form
    - Added a new file `route.ts` in the `app/api/courses/[courseId]/access` directory to handle PATCH requests for updating course access.
    - Created a new component `CourseAccessForm` in the `components/dashboard/teacher/courses/courseId` directory to handle the course access form.
    - Updated the `CourseIdPage` component to include the `CourseAccessForm` component.
    - Removed the `PlanForm` component from the `CourseIdPage` component.
    - Updated the `PriceForm` component to handle courses with free access and display the appropriate message.
    - Updated the `Course` model in the Prisma schema to include the `isFree` field.

--- components/dashboard/teacher/courses/courseId/PriceForm.tsx
+++ components/dashboard/teacher/courses/courseId/PriceForm.tsx
@@ -32,7 +32,11 @@ const formSchema = z.object({
   price: z.coerce.number(),
 });
 
-export default function PriceForm({ initialData, courseId, isDeleting }: PriceFormProps) {
+export default function PriceForm({
+  initialData,
+  courseId,
+  isDeleting,
+}: PriceFormProps) {
   const [isEditing, setIsEditing] = useState(false);
 
   const toggleEdit = () => setIsEditing((current) => !current);
@@ -60,10 +64,10 @@ export default function PriceForm({ initialData, courseId, isDeleting }: PriceFo
   };
 
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
-      <div className="font-medium flex items-center justify-between">
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
         Course price
-        {isDeleting ? (
+        {isDeleting || initialData.isFree ? (
           <Ban className="h-4 w-4" />
         ) : (
           <Button onClick={toggleEdit} variant="ghost">
@@ -71,7 +75,7 @@ export default function PriceForm({ initialData, courseId, isDeleting }: PriceFo
               <>Cancel</>
             ) : (
               <>
-                <Pencil className="h-4 w-4 mr-2" />
+                <Pencil className="mr-2 h-4 w-4" />
                 Edit price
               </>
             )}
@@ -81,45 +85,50 @@ export default function PriceForm({ initialData, courseId, isDeleting }: PriceFo
       {!isEditing && (
         <p
           className={cn(
-            "text-sm mt-2",
-            !initialData.price && "text-slate-500 italic"
+            "mt-2 text-sm",
+            !initialData.price && "italic text-slate-500",
           )}
         >
-          {initialData.price ? formatPrice(initialData.price) : "No price"}
+          {initialData.price
+            ? formatPrice(initialData.price)
+            : initialData.isFree
+              ? "This course is free"
+              : "No price"}
         </p>
       )}
-      {isEditing && (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      step="0.01"
-                      disabled={isSubmitting}
-                      placeholder="Set a price for your course"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center gap-x-2">
-              <Button disabled={!isValid || isSubmitting} type="submit">
-                Save
-              </Button>
-            </div>
-          </form>
-        </Form>
-      )}
+      {isEditing &&
+        !initialData.isFree &&(
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="mt-4 space-y-4"
+            >
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        step="0.01"
+                        disabled={isSubmitting}
+                        placeholder="Set a price for your course"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center gap-x-2">
+                <Button disabled={!isValid || isSubmitting} type="submit">
+                  Save
+                </Button>
+              </div>
+            </form>
+          </Form>
+        )}
     </div>
   );
 }
