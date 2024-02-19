bdb9f593816c38f47480df0bb4b11d48b2011668
bryanaim <isale.bryan@gmail.com>
2024-02-19 20:07:18 +0300

    Update color and background image

--- components/index/Stats.tsx
+++ components/index/Stats.tsx
@@ -4,13 +4,13 @@ import CountUp from "react-countup";
 
 const Stats = () => {
   return (
-    <section className="bg-[url('/index/bg_1.jpg')] bg-cover bg-center bg-no-repeat">
+    <section className="bg-[url('/../images/stats.jpg')] bg-cover bg-center bg-no-repeat">
       <div className="bg-[rgb(0,0,0)] opacity-70">
         <MaxWidthWrapper>
           <div className="h-[32rem] items-center justify-between gap-2 py-16 md:flex">
             <div className="flex basis-1/3 items-center gap-4 text-white">
               <div className="text-6xl">
-                <div className="border-r-4 border-red-800">
+                <div className="border-r-4 border-pes-red">
                   <CountUp
                     end={8}
                     duration={5}
@@ -26,7 +26,7 @@ const Stats = () => {
                 end={100}
                 duration={5}
                 suffix="+"
-                className="basis-25 border-r-4 border-red-800 text-6xl font-semibold"
+                className="basis-25 border-r-4 border-pes-red text-6xl font-semibold"
               />
               <p className=" ps-4 text-3xl ">Students</p>
             </div>
