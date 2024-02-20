0bd3015e045b9f9d5180d6a24d62c8f5e8c01988
bryanaim <isale.bryan@gmail.com>
2024-02-20 13:33:39 +0300

    Update page layout with new components

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
-import Benefits from "@/components/index/Benefits";
 import CTA from "@/components/index/CTA";
-import CarouselWrapper from "@/components/index/CarouselWrapper";
 import ComingSoon from "@/components/index/ComingSoon";
 import Instructors from "@/components/index/Instructors";
 import PopularCourses from "@/components/index/PopularCourses";
-import SelfSelection from "@/components/index/SelfSelection";
 import Stats from "@/components/index/Stats";
-import Welcome from "@/components/index/Welcome";
+import Intro from "@/components/index/Intro"
+import Description from "@/components/index/Description"
 
 export const metadata: Metadata = {
   title: "Homepage",
@@ -21,14 +19,8 @@ export default function Index() {
   return (
     <div>
       <main className=" flex flex-col ">
-        <div className="relative mb-48">
-          <CarouselWrapper />
-          <div className="absolute left-0 right-0 -translate-y-1/2 transform">
-            <SelfSelection />
-          </div>
-        </div>
-        <Welcome />
-        <Benefits />
+        <Intro />
+        <Description />
         <PopularCourses />
         <Instructors />
         <Stats />
