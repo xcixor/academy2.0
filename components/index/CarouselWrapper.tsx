cde0b1b71491ffab504b971883e45193035f5bdb
Brian Isale <isale.bryan@gmail.com>
2024-02-29 13:31:00 +0300

    update images for carousels

--- components/index/CarouselWrapper.tsx
+++ components/index/CarouselWrapper.tsx
@@ -28,20 +28,20 @@ type Props = {
 };
 const images = [
   {
-    src: "/carousel/photo-1.avif",
+    src: "/carousel/carousel1.jpg",
     title: "Explore Excellence",
     legend:
       "Dive into our curated selection of featured courses at PES Academy. From foundational principles to advanced strategies, discover transformative learning experiences that elevate your understanding on various topics.",
     alt: "Photo 1",
   },
   {
-    src: "/carousel/photo-2.avif",
+    src: "/carousel/carousel2.jpg",
     title: "Guiding Experts",
     legend: "Our seasoned instructors bring real-world insights and expertise, ensuring a rich and engaging learning journey. Discover the mentors shaping your path to success.",
     alt: "Photo 2",
   },
   {
-    src: "/carousel/photo-3.avif",
+    src: "/carousel/carousel3.jpg",
     title: "Practical Insights for Today's Challenges",
     legend: "Our practical insights carousel offers courses designed to address today's challenges. Equip yourself with the knowledge to thrive in a rapidly evolving financial landscape.",
     alt: "Photo 3",
@@ -88,11 +88,12 @@ const CarouselWrapper = () => {
             width={800}
             alt={image.alt}
             className="h-[600px] w-full"
+            style={{ objectFit: "cover" }}
           />
           <div className="carousel-text-wrapper absolute bottom-0 h-full w-full text-white">
             <div className="flex h-full flex-col items-center justify-center">
               <div className="w-1/2">
-                <h2 className="animate-fadeIn text-4xl font-bold">
+                <h2 className="animate-fadeIn text-4xl font-bold mb-5">
                   {image.title}
                 </h2>
                 <p className="animate-left font-semibold">{image.legend}</p>
