47c1ee9c5948bbe3938b3ce1e6c44a5b0c90108a
xcixor <pndungu54@gmail.com>
2024-03-13 12:33:01 +0300

    Refactor document review page and document upload page
    - Refactored the document review page and document upload page to improve code organization and readability.
    - Added a back button to both pages for easier navigation.
    - Updated the routing for creating a new document review document.
    - Removed commented out code for creating a document review document.
    - Fixed a bug where the routing for creating a new document review document was incorrect.

--- app/dashboard/(routes)/document-review/[reviewId]/docs/[docId]/page.tsx
+++ app/dashboard/(routes)/document-review/[reviewId]/docs/[docId]/page.tsx
@@ -1,5 +1,7 @@
 import { getLatestFileMetaData } from "@/actions/get-latest-file-metadata";
 import UploadReviewDocumentForm from "@/components/dashboard/clients/docs/UploadReviewDocumentForm";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -13,6 +15,15 @@ const page = async ({
 
   return (
     <div className="p-12">
+      <div className="flex justify-between align-middle">
+        <Link
+          href={`/dashboard/document-review/${params.reviewId}`}
+          className="flex items-center text-sm transition hover:opacity-75"
+        >
+          <ArrowLeft className="mr-2 h-4 w-4" />
+          Back
+        </Link>
+      </div>
       <UploadReviewDocumentForm
         assetId={params.docId}
         isDeleting={false}
