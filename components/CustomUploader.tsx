6a8ed0cd837aa0587d643c05f8dd8e3aeaa4a9a0
Brian Isale <isale.bryan@gmail.com>
2024-03-25 10:49:05 +0300

    create image validation so that images are under 500kb

--- components/CustomUploader.tsx
+++ components/CustomUploader.tsx
@@ -9,13 +9,15 @@ const CustomUploader = (props: Props) => {
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      setFile(e.target.files[0]);
+      const selectedFile = e.target.files[0];
+      // Check if file size exceeds 500KB
+      if (selectedFile.size > 500 * 1024) {
+        alert("File size should not exceed 500KB.");
+        return; // Prevent setting the file and exit the function
+      }
+      setFile(selectedFile);
     }
   };
-
-  //   const handleUpload = async () => {
-  //     // We will fill this out later
-  //   };
 
   const handleUpload = async () => {
     if (file) {
@@ -38,15 +40,15 @@ const CustomUploader = (props: Props) => {
       xhr.addEventListener("readystatechange", function (e) {
         if (xhr.readyState === 4 && xhr.status == 200) {
           console.log("Doneeee");
+          // Optionally, reset the file state or perform additional actions on success
         } else if (xhr.readyState === 4 && xhr.status != 200) {
           console.log("errred", xhr.status, xhr.responseText);
         }
       });
-      var formData = new FormData();
-      formData.append("files", file);
-      xhr.send(formData);
+      xhr.send(file);
     }
   };
+
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
